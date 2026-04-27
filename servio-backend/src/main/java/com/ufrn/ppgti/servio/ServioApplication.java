eeebcb5deb066ba948a29c2984fea00a23514bb1
Maria Clara Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2026-04-27 14:19:09 -0300

    Criando projetos

---
+++ servio-backend/src/main/java/com/ufrn/ppgti/servio/ServioApplication.java
@@ -0,0 +1,13 @@
+package com.ufrn.ppgti.servio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServioApplication.class, args);
+	}
+
+}
---
+++ servio-backend/src/test/java/com/ufrn/ppgti/servio/ServioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufrn.ppgti.servio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
